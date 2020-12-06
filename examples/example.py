859d972fbfc6be9f04d6d3cb80cc5367f4c5327d
ramenguy99 <ramenguy99@gmail.com>
2020-12-06 12:56:01 +0000

    List of urls as arg to stack constructor

--- examples/example.py
+++ examples/example.py
@@ -2,29 +2,39 @@
 
 import pycoxnet
 
-s = pycoxnet.stack("null://")
-
-print("repr " + repr(s))
-print("str " + str(s))
-print("getstack " + hex(s.getstack()))
+s = pycoxnet.stack("picox", ["null://", "null://"])
 
 if_index = s.if_nametoindex("vde0")
-print("vde0 index: ", if_index)
+if_index = s.if_nametoindex("vde1")
+
 try:
-    print("bananas index: ", s.if_nametoindex("bananas"))
-except Exception as e:
-    print(str(e))
+    if_index = s.if_nametoindex("vde2")
+    print("Error!")
+except:
+    pass
+    
 
-
-print(pycoxnet.AF_INET);
-print(hex(pycoxnet.htonl(0xFF)))
-
-addr = pycoxnet.inet_pton(pycoxnet.AF_INET, "10.0.0.42")
-print(addr)
-s.ipaddr_add(pycoxnet.AF_INET, addr, 24, if_index)
-
-addr = pycoxnet.inet_pton(pycoxnet.AF_INET6, "::42");
-print(addr)
-s.ipaddr_add(pycoxnet.AF_INET6, addr, 64, if_index)
+# print("repr " + repr(s))
+# print("str " + str(s))
+# print("getstack " + hex(s.getstack()))
+# 
+# if_index = s.if_nametoindex("vde0")
+# print("vde0 index: ", if_index)
+# try:
+#     print("bananas index: ", s.if_nametoindex("bananas"))
+# except Exception as e:
+#     print(str(e))
+# 
+# 
+# print(pycoxnet.AF_INET);
+# print(hex(pycoxnet.htonl(0xFF)))
+# 
+# addr = pycoxnet.inet_pton(pycoxnet.AF_INET, "10.0.0.42")
+# print(addr)
+# s.ipaddr_add(pycoxnet.AF_INET, addr, 24, if_index)
+# 
+# addr = pycoxnet.inet_pton(pycoxnet.AF_INET6, "::42");
+# print(addr)
+# s.ipaddr_add(pycoxnet.AF_INET6, addr, 64, if_index)
 
 
